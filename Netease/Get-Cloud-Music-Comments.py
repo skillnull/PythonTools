1c69550825aeb04dfeabcd31b73571471c583fdc
skillnull <skillisnull@gmail.com>
2019-07-23 17:55:22 +0800

    remove cookie

--- Netease/Get-Cloud-Music-Comments.py
+++ Netease/Get-Cloud-Music-Comments.py
@@ -40,14 +40,7 @@ header = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Connection': 'keep-alive',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,da;q=0.7',
-    'Cookie': '_iuqxldmzr_=32; _ntes_nnid=0e6e1606eb78758c48c3fc823c6c57dd,1527314455632; '
-              '_ntes_nuid=0e6e1606eb78758c48c3fc823c6c57dd; __utmc=94650624; __utmz=94650624.1527314456.1.1.'
-              'utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WM_TID=blBrSVohtue8%2B6VgDkxOkJ2G0VyAgyOY;'
-              ' JSESSIONID-WYYY=Du06y%5Csx0ddxxx8n6G6Dwk97Dhy2vuMzYDhQY8D%2BmW3vlbshKsMRxS%2BJYEnvCCh%5CKY'
-              'x2hJ5xhmAy8W%5CT%2BKqwjWnTDaOzhlQj19AuJwMttOIh5T%5C05uByqO%2FWM%2F1ZS9sqjslE2AC8YD7h7Tt0Shufi'
-              '2d077U9tlBepCx048eEImRkXDkr%3A1527321477141; __utma=94650624.1687343966.1527314456.1527314456'
-              '.1527319890.2; __utmb=94650624.3.10.1527319890'
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,da;q=0.7'
 }
 # 设置代理服务器
 proxies = ip_pool
