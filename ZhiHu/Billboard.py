e08e0d97ee16bc7f7d8bfcf742bc9ef3927ddfb8
skillnull <skillisnull@gmail.com>
2024-11-08 11:14:42 +0800

    feat: update

--- ZhiHu/Billboard.py
+++ ZhiHu/Billboard.py
@@ -2,12 +2,16 @@
 
 import requests
 from bs4 import BeautifulSoup
+import json
 
 def billboard():
   url = 'https://www.zhihu.com/billboard'
   res = requests.get(url)
   soup = BeautifulSoup(res.text, 'lxml')
-  titles = soup.find_all('div', class_='HotList-itemTitle')
+  data = soup.find('script',id='js-initialData').get_text()
+  hotList = json.loads(data)['initialState']['topstory']['hotList']
+  # print(json.dumps(hotList, indent=2, ensure_ascii=False))
 
-  for title in titles:
-    print(title.text)
+  for item in hotList:
+    print(item['target']['titleArea']['text'])
+    print('\r\n\t--------', item['target']['excerptArea']['text'], '\r\n')
